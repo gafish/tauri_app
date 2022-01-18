76b525558cfc06da5fac2c354ec00e94f3236390
徐辉 <hui.xu@dingxiang-inc.com>
2022-01-18 17:07:35 +0800

    dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import DialogOpen from './dialog/open'
-import DialogSave from './dialog/save'
+import Dialog from './dialog'
 
 function App() {
   return (
     <div style={{ padding: 10 }}>
-      <div>
-        <h2>dialog</h2>
-        <DialogOpen />
-        <DialogSave />
-      </div>
+      <Dialog />
     </div>
   )
 }
---
+++ src/dialog/index.tsx
@@ -0,0 +1,14 @@
+import DialogOpen from './open'
+import DialogSave from './save'
+
+const App = () => {
+  return (
+    <div>
+      <h2>dialog</h2>
+      <DialogOpen />
+      <DialogSave />
+    </div>
+  )
+}
+
+export default App
